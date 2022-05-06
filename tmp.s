e9a1209d732721359a572d45038fc1d39fb54622
Focus-Sash <tenma.quo.ast@gmail.com>
2022-05-06 10:27:43 +0900

    Implement multiplication and division.

--- tmp.s
+++ tmp.s
@@ -1,7 +1,16 @@
 .intel_syntax noprefix
 .globl main
 main:
-  mov rax, 12
-  add rax, 34
-  sub rax, 5
+  push 5
+  push 6
+  push 7
+  pop rdi
+  pop rax
+  imul rax, rdi
+  push rax
+  pop rdi
+  pop rax
+  add rax, rdi
+  push rax
+  pop rax
   ret
